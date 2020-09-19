b17a98005e02aec3d2044e274b9226494357f0d4
Rakin Mohammed <rakinmohammed1@gmail.com>
2020-09-19 19:56:53 -0400

    tets

--- Clay/Math.h
+++ Clay/Math.h
@@ -2,8 +2,17 @@
 
 class Math {
     public:
-        Math() {
-
+        Math(){}
+        
+        Transformation multiply(Transformation m1, Transformation m2) {
+            Transformation matrix; 
+            for (int c = 0; c < 4; c++) {
+                for (int r = 0; r < 4; r++) {
+                    matrix.matrix[r][c] = m1.matrix[r][0] * m2.matrix[0][c] + m1.matrix[r][1] * m2.matrix[1][c] + m1.matrix[r][2] * m2.matrix[2][c]
+                                 + m1.matrix[r][3] * m2.matrix[3][c];
+                }
+            }
+            return matrix;
         }
 
         Vertex crossProduct(Vertex vector1, Vertex vector2)
